51df7d54c513712c699685bab3c85605e232fdd1
Shorabul Hoque <shorabulhoque@yahoo.com>
2025-08-27 16:21:51 +0200

    Call history display functionality added

--- script/home.js
+++ script/home.js
@@ -7,11 +7,14 @@ function setInnerTextById(id, text) {
 function getInnerTextByClassName(className) {
     return document.getElementsByClassName(className).innerText;
 }
+function getElementByID(id) {
+    return document.getElementById(id);
+}
 document.getElementById('card-container').addEventListener('click', function (e) {
     //heart
     if (e.target.className.includes('heart-icon')) {
-        let numberOFHearts = getInnerTextByID('hearts-number');
-        let currentNumberOfHearts = Number(getInnerTextByID('hearts-number')) + 1;
+        const numberOFHearts = getInnerTextByID('hearts-number');
+        const currentNumberOfHearts = Number(getInnerTextByID('hearts-number')) + 1;
         setInnerTextById('hearts-number', currentNumberOfHearts);
     }
     //call
@@ -21,9 +24,23 @@ document.getElementById('card-container').addEventListener('click', function (e)
             alert(`Calling ${e.target.parentNode.parentNode.children[2].innerText} ${e.target.parentNode.parentNode.children[3].innerText}...`);
             coinNumber -= 20;
             setInnerTextById('coin-number', coinNumber);
+            const newCallHistory = document.createElement('div');
+            newCallHistory.innerHTML = `
+            <div class="bg-[#fafafa] rounded-lg p-4 flex justify-between items-center">
+                <div>
+                    <h3 class="text-[#111111]">${e.target.parentNode.parentNode.children[1].innerText}</h3>
+                    <p>${e.target.parentNode.parentNode.children[3].innerText}</p>
+                </div>
+                <p>11:26.58 AM</p>
+            </div>
+            `;
+            getElementByID('call-history-container').appendChild(newCallHistory);
         } else {
             alert(`Call unavailable! Please make sure you have 20 or more coins.`);
         }
     }
 });
 
+getElementByID('clear-btn').addEventListener('click', function () {
+    getElementByID('call-history-container').innerText = '';
+});
